0affdd2e09ecf9dacb2fa2a7f4e3802a831f8393
ArtiolaDomiCaka <artioladomi@hotmail.com>
2021-11-22 23:26:14 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//With the usual Users and Todo lists:
+
+// - Find all the users who's name contains the letter 
+// - Say hi to those users in the console - but only 1 greeting every 2 seconds.
+// - Prompt the user for a user ID. Using the value, find all the todos that are Inc
+
+
+// - Prompt the user for a letter
+
+const userLetter = prompt("Input a letter")
+
+
+
+
+
+
