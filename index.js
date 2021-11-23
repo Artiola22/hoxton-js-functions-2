26c6f6c9bc237f896ef9237fa108acd457a52b99
ArtiolaDomiCaka <artioladomi@hotmail.com>
2021-11-23 15:29:29 +0100

    Excercise completion

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //With the usual Users and Todo lists:
 
-// - Find all the users who's name contains the letter 
+
 // - Say hi to those users in the console - but only 1 greeting every 2 seconds.
 // - Prompt the user for a user ID. Using the value, find all the todos that are Inc
 
@@ -9,8 +9,35 @@
 
 const userLetter = prompt("Input a letter")
 
+// - Find all the users who's name contains the letter 
 
+const foundUsers = users.filter(function (user){
+    return user.name.includes(userLetter)
+})
 
+console.log('foundUsers:', foundUsers)
+let userIndex = 0
 
+// setInterval :: (function, ms: number) => id: number
+if (foundUsers.length>0) {
+    const intervalId = setInterval(function () {
+        const user = foundUsers[userIndex]
+        console.log(`Hello, ${user.name}!`)
+        userIndex += 1
+        if (userIndex === foundUsers.length) clearInterval(intervalId)
+    }, 2000)
+    } else{
+        console.log('Found no users....')
+    }
+// setTimeout :: (function, ms: number) => id: number
+
+const timeoutId = setTimeout(function(){
+
+}, 1000)
+
+// setInterval :: (function, ms: number) => id: number
+const intervalId = setInterval(function (){
+
+}, 1000)
 
 
